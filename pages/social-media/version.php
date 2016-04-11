1227bf2e844f94daffdb20160d2502561985ce3b
Timothy Gibbons <Root3287@users.noreply.github.com>
2016-04-11 14:36:35 -0500

    Fix issue and Add more api
    Fix version bug,
    add more api json

--- pages/social-media/version.php
+++ pages/social-media/version.php
@@ -5,16 +5,25 @@ $return = [
 	'message' => "",
 ];
 if(Input::exists('get')){
-	if(Input::get('version') != "" && Input::get('uid') != ""){
-		if(version_compare($CURRENT, escape(Input::get('version'), '<'))){ //There is an update.
+	if(Input::get('version') != "" && Input::get('uid') != "" && strlen(escape(Input::get('uid')))){
+		if(version_compare($CURRENT, escape(Input::get('version'))) == 1){ //There is an update.
+			$return['code'] = 0;
 			$return['update'] = true;
 			$return['message'] = "There is an update advailable";
 			$return['old_version'] = escape(Input::get('version'));
 			$return['new_version'] = $CURRENT;
+			$return['uid'] = escape(Input::get('uid'));
 			echo json_encode($return);
-		}else{
+		}else if(version_compare($CURRENT, escape(Input::get('version'))) == 0){ // Up to date.
+			$return['code'] = 0;
+			$return['uid'] = escape(Input::get('uid'));
 			$return['update'] = false;
 			$return["message"] = "There is not an update yet";
+			echo json_encode($return);
+		}else{
+			$return['code'] = 3; // Higher version
+			$return['message'] = "You have a newer version than the server. This may have some issues."
+			$return['update'] = false;
 			echo json_encode($return);
 		}
 	}else{
