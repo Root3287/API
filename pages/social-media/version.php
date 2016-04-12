943629eec4478acf0b3bc27721db170e062ed64d
Timothy Gibbons <Root3287@users.noreply.github.com>
2016-04-11 23:52:02 -0500

    Update version.php

--- pages/social-media/version.php
+++ pages/social-media/version.php
@@ -1,29 +1,32 @@
 <?php
-$CURRENT = "1.1.2.3";
+$CURRENT = "0.0.1";
+$DL_URL = "https://github.com/Root3287/Social-Media/archive/master.zip";
 $return = [
 	'code'=>0, 
 	'message' => "",
 ];
 if(Input::exists('get')){
-	if(Input::get('version') != "" && Input::get('uid') != "" && strlen(escape(Input::get('uid')))){
+	if(Input::get('version') != "" && Input::get('uid') != "" && strlen(Input::get('uid')) == 62){
 		if(version_compare($CURRENT, escape(Input::get('version'))) == 1){ //There is an update.
 			$return['code'] = 0;
 			$return['update'] = true;
 			$return['message'] = "There is an update advailable";
-			$return['old_version'] = escape(Input::get('version'));
+			$return['version'] = escape(Input::get('version'));
 			$return['new_version'] = $CURRENT;
+			$return['download_url'] = $DL_URL;
 			$return['uid'] = escape(Input::get('uid'));
 			echo json_encode($return);
-		}else if(version_compare($CURRENT, escape(Input::get('version'))) == 0){ // Up to date.
+		}else if(version_compare($CURRENT, escape(Input::get('version'))) == 0){
 			$return['code'] = 0;
 			$return['uid'] = escape(Input::get('uid'));
 			$return['update'] = false;
+			$return['version'] = escape(Input::get('version'));
 			$return["message"] = "There is not an update yet";
 			echo json_encode($return);
 		}else{
-			$return['code'] = 3; // Higher version
-			$return['message'] = "You have a newer version than the server. This may have some issues."
 			$return['update'] = false;
+			$return['message'] = "You have a newer version than what the server has. This may have some issues.";
+			$return['code'] = 3; //New Version
 			echo json_encode($return);
 		}
 	}else{
